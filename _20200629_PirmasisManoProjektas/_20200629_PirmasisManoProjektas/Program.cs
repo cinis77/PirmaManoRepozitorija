2aa233bc296b0bea00810b59ad25893f60ce02aa
cinis77 <cinis.dwarnius.minis@gmail.com>
2020-06-29 19:07:01 +0300

    Pirmasis mano commit
    Sukurtas projektas, įkeliamas pirminis projektas ir visi atitinkami failai

---
+++ _20200629_PirmasisManoProjektas/_20200629_PirmasisManoProjektas/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20200629_PirmasisManoProjektas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sveikas pasauli");
+        }
+    }
+}
